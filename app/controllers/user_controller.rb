2c727ec374925a34f79696b2c34c3d25212b2370
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-06-11 10:13:03 +0200

    adds password encription

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class UserController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
@@ -5,11 +7,13 @@ class UserController < ApplicationController
     
     if @user.valid? 
       existing_user = User.find_by(:email => params[:email])
-      if(existing_user.present?)
+      if existing_user.present?
         render json: { message: "User with Email: " + params[:email] + " already exist" }, status: 401
       else
+        encripted_password = BCrypt::Password.create(params[:password])
+        @user.password = encripted_password
         @user.save
-        render json: @user, status: 201
+        render json: { message: "Signup succesful", id: @user[:id], email: @user[:email] }, status: 201
       end
     else
       render json: @user.errors.details, status: 500
