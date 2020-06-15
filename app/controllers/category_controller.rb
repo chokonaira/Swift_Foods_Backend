6b118cf6274bb15d551774f11172722c7aec5a37
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-06-15 18:20:18 +0200

    adds token authorization to non auth routes

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,22 @@
 class CategoryController < ApplicationController
   def add
+    @category = Category.new(category_param)
+
+    if @category.valid?
+      existing_category = Category.find_by(:name => params[:name])
+      if existing_category.present?
+        render json: { message: "Category: " + params[:name] + " already exist" }, status: 401
+      else
+        @category.save
+        render json: { message: "Category: " + params[:name] + ", added succesfully"}, status: 201
+      end
+    else
+      render json: @category.errors.details, status: 500
+    end
+  end
+
+  private
+  def category_param
+    params.require(:category).permit(:name)
   end
 end
