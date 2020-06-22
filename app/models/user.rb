51746e8d1493c801a3a0b37d8fabcf877d9b7f7b
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-06-22 17:52:26 +0200

    fixed existing user validation for invalid credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ApplicationRecord
+  before_create do
+    self.password = BCrypt::Password.create(password) if password
+  end
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, :uniqueness => true
   validates :password, presence: true
   validates :phone, presence: true
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && BCrypt::Password.new(user.password)==password
+      return user
+    end
+    nil
+  end
 end
