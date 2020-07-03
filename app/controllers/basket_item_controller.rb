621555c01d02808c48320139143675512943c075
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-07-03 18:59:30 +0200

    completes adds basket item endpoint

--- app/controllers/basket_item_controller.rb
+++ app/controllers/basket_item_controller.rb
@@ -2,14 +2,14 @@ class BasketItemController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def add_basket_item
-    basket = BasketItem.new(basket_item_params)
-    
-    if basket.valid?
-       basket.save
-      
-      render json: { message: "Product added to Basket", basket_item: basket }, status: 201
+    existing_basket_product = BasketItem.find_by(:product_id => params[:product_id])
+    if existing_basket_product.present?
+      existing_basket_product.update(:quantity => params[:quantity])
+      render json: { message: "Product quantity updated in Basket", basket_item: existing_basket_product }, status: 200
     else
-      render json: basket_item.errors.details, status: 401
+      basket_item = BasketItem.new(basket_item_params)
+      basket_item.save
+      render json: { message: "Product added to Basket", basket_item: basket_item }, status: 201
     end
   end
 
